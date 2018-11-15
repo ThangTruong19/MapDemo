7ed335bfa599103ed828a87e535be57d5d67c69f
ThangTruong <tuyenamai@gmail.com>
2018-11-15 13:09:26 +0700

    Map marker and windows info

--- app/src/main/java/com/thangtruong19/mapdemo/MapsActivity.java
+++ app/src/main/java/com/thangtruong19/mapdemo/MapsActivity.java
@@ -16,6 +16,7 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
 import android.widget.AutoCompleteTextView;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -23,10 +24,15 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.AutocompletePrediction;
 import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBuffer;
 import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -37,6 +43,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.thangtruong19.mapdemo.model.PlaceInfo;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -57,10 +64,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private static final String TAG="MapActivity";
 
     private AutoCompleteTextView searchText;
-    private ImageView mGps;
+    private ImageView mGps,mInfo;
     private PlaceAutoCompleteAdapter placeAutoCompleteAdapter;
     protected GeoDataClient mGeoDataClient;
     private static final LatLngBounds LAT_LNG_BOUNDS=new LatLngBounds(new LatLng(-40,-168),new LatLng(71,136));
+    private PlaceInfo mPlace;
+    private Marker mMarker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -70,7 +79,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             checkLocationPermission();
         }
         searchText=findViewById(R.id.input_search);
+        searchText.setOnItemClickListener(mAutoCompleteClickListener);
+
         mGps=findViewById(R.id.ic_gps);
+        mInfo=findViewById(R.id.place_info);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -114,6 +127,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 }
             }
         });
+        mInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG,"onclick: clicked place info");
+                try{
+                    if(mMarker.isInfoWindowShown()){
+                        mMarker.hideInfoWindow();
+                    }else{
+                        Log.d(TAG,"onClick: place info : "+mPlace.toString());
+                        mMarker.showInfoWindow();
+                    }
+                }catch (NullPointerException e){
+                    Log.e(TAG,"onClick: NullPointerException: "+e.getMessage());
+                }
+            }
+        });
     }
 
     private void geoLocate(){
@@ -134,7 +163,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             Log.d(TAG,"found a location: "+address.toString());
 
             LatLng latLng=new LatLng(address.getLatitude(),address.getLongitude());
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,20));
 
             MarkerOptions options=new MarkerOptions()
                     .position(latLng)
@@ -204,6 +233,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .addApi(LocationServices.API)
+                .addApi(Places.GEO_DATA_API)
                 .build();
        // client.connect();
     }
@@ -225,7 +255,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         currentLocationMarker=mMap.addMarker(markerOptions);
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,20));
         //mMap.animateCamera(CameraUpdateFactory.zoomBy(10));
         if(client!=null){
             LocationServices.FusedLocationApi.removeLocationUpdates(client,this);
@@ -261,5 +291,73 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
+    }
+
+    private AdapterView.OnItemClickListener mAutoCompleteClickListener=new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            final AutocompletePrediction item=placeAutoCompleteAdapter.getItem(position);
+            final String placeId=item.getPlaceId();
+
+            PendingResult<PlaceBuffer> placeResult=Places.GeoDataApi
+                    .getPlaceById(client,placeId);
+            placeResult.setResultCallback(mUpdatePlaceDetailsCallback);
+        }
+    };
+
+    private ResultCallback<PlaceBuffer> mUpdatePlaceDetailsCallback=new ResultCallback<PlaceBuffer>() {
+        @Override
+        public void onResult(@NonNull PlaceBuffer places) {
+            if(!places.getStatus().isSuccess()){
+                Log.d(TAG,"onResult: Place query did not complete successfully"+places.getStatus().toString());
+                places.release();
+                return;
+            }
+            final Place place=places.get(0);
+
+            try {
+                mPlace = new PlaceInfo(place.getName().toString(), place.getAddress().toString(), place.getPhoneNumber().toString()
+                        , place.getId(), place.getWebsiteUri(), place.getLatLng(), place.getRating());
+
+                Log.d(TAG, "onResult: place details: " + mPlace.toString());
+            }catch (NullPointerException e){
+                Log.e(TAG,"onResult: NullPointerException: "+e.getMessage());
+            }
+            /*mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mPlace.getLatLng(),20));
+            MarkerOptions options=new MarkerOptions()
+                    .position(mPlace.getLatLng())
+                    .title(place.getName().toString());
+
+            mMap.addMarker(options);*/
+            moveCamera(mPlace.getLatLng(),20,mPlace);
+            places.release();
+
+        }
+    };
+
+    private void moveCamera(LatLng latLng,int zoom,PlaceInfo placeInfo){
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,zoom));
+
+        mMap.clear();
+
+        if(placeInfo!=null){
+            try{
+                String snippet="Address: "+placeInfo.getAddress()+"\n"
+                        +"Phone Number: "+placeInfo.getPhoneNumber()+"\n"
+                        +"Website: "+placeInfo.getWebsiteUri()+"\n"
+                        +"Price Rating: "+placeInfo.getRating()+"\n";
+
+                MarkerOptions options=new MarkerOptions()
+                        .position(latLng)
+                        .title(placeInfo.getName())
+                        .snippet(snippet);
+
+                mMarker=mMap.addMarker(options);
+            }catch (NullPointerException e){
+                Log.e(TAG,"moveCamera: NullPointerException: "+e.getMessage());
+            }
+        }else {
+            mMap.addMarker(new MarkerOptions().position(latLng));
+        }
     }
 }
